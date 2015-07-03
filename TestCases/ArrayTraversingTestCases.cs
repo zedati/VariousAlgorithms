63bbc2099aa020f390219dd20cd1f7b798b88fd8
Nikita Veshkurtsev <nikita.veshkurtsev@bandlab.com>
2015-07-03 18:48:21 +0800

    Find deviding element in the array

---
+++ TestCases/ArrayTraversingTestCases.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using VariousAlgorithms;
+using Xunit;
+
+namespace TestCases
+{
+    public class ArrayTraversingTestCases
+    {
+        [Theory]
+        [InlineData(null, 1, -1)]
+        [InlineData(new int[] {}, 1, -1)]
+        [InlineData(new[] { 1, 1, 1, 1, 1 }, 1, -1)]    // array of same elements and x is equal to every element
+        [InlineData(new[] { 1, 1, 1, 1, 1 }, 2, -1)]    // array of same elements and x is not equal to any element
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 1, 8)]
+        [InlineData(new[] { 4, 7, 1, 3, 9, 1, 8, 1, 6, 5, 4, 3, 1, 0 }, 1, 9)]
+        [InlineData(new[] { 1, 8, 1, 6, 5, 4, 3, 1, 0 }, 1, 5)]
+        [InlineData(new[] { 5, 5, 3, 1, 6, 5, 1 }, 5, 3)]
+        [InlineData(new[] { 5, 5, 3, 1, 6, 5, 1 }, 3, 5)]
+        public void ArrayDevidingElement(int[] a, int x, int expResult)
+        {
+            var actualResult = ArrayTraversing.FindDevidingElement(a, x);
+
+            Assert.Equal(expResult, actualResult);
+        }
+    }
+}
---
+++ VariousAlgorithms/ArrayTraversing.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousAlgorithms
+{
+    public class ArrayTraversing
+    {
+        /// <summary>
+        /// An array of N elements and number x are given. 
+        /// It's necessary to find such deviding element, that the number of element equal to x in left part 
+        /// and the number of element not equal to x in the right part are equal
+        /// For example, for an array a = {5, 5, 3, 1, 6, 5, 1} and x = 5
+        /// the dividing element will be a[3] = 1, because the number of elements equal to 5 in a[0..3] and
+        /// the number of elements not equal to 5 in a[4..6] are the same.
+        /// </summary>
+        /// <param name="a">The given array of integers.</param>
+        /// <param name="x">The given number.</param>
+        public static int FindDevidingElement(int[] a, int x)
+        {
+            if (a == null || a.Length == 0) return -1;
+
+            int i = 0, j = a.Length - 1, l = 0, r = 0;
+
+            while (true)
+            {
+                if (a[i] == x) l++;
+                if (a[j] != x) r++;
+
+                while (l < r && i != j)
+                {
+                    i++;
+                    if (a[i] == x) l++;
+                }
+
+                while (l > r && i != j)
+                {
+                    j--;
+                    if (a[j] != x) r++;
+                }
+
+                if (i == j) break;
+
+                i++; j--;
+            };
+
+            if (l == 0 || r == 0)
+                return -1;
+
+            return i;
+        }
+    }
+}
